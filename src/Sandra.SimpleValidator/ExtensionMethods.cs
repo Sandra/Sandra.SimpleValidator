678a8ac1ae473603b42e6722b9d4263582442cf8
Phillip Haydon <phillip.haydon@gmail.com>
2013-01-24 01:20:08 +0800

    fixed problem trying to get property name

---
+++ src/Sandra.SimpleValidator/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Sandra.SimpleValidator
+{
+    public static class ExtensionMethods
+    {
+        public static string GetCorrectPropertyName<T>(this Expression<Func<T, dynamic>> expression)
+        {
+            if (expression.Body is MemberExpression)
+            {
+                return ((MemberExpression)expression.Body).Member.Name;
+            }
+            else
+            {
+                var op = ((UnaryExpression)expression.Body).Operand;
+                return ((MemberExpression)op).Member.Name;
+            }
+        }
+    }
+}
