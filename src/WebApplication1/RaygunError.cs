1b7fbb92b2ad9d2dac2c0f9362fe7b42cbec4c2a
Phillip Haydon <phillip.haydon@gmail.com>
2013-01-24 01:22:38 +0800

    deleted empty class and old validation base

---
+++ src/WebApplication1/RaygunError.cs
@@ -0,0 +1,20 @@
+﻿using Mindscape.Raygun4Net;
+using Nancy.Bootstrapper;
+
+namespace WebApplication1
+{
+public class RaygunNancyRegistration : IApplicationStartup
+{
+    public void Initialize(IPipelines pipelines)
+    {
+        var client = new RaygunClient();
+
+        pipelines.OnError.AddItemToEndOfPipeline((context, exception) =>
+        {
+            client.Send(exception);
+
+            return null;
+        });
+    }
+}
+}
